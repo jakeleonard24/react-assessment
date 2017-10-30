54920f9a2ffcbfa874a2a3de8c9e5778717d75b0
Jake Leonard <jake24leonard@gmail.com>
2017-10-29 19:54:36 -0700

    patched to handle refresh, Did with modal instead if route but I've done a million routes and modals are cool

--- client/src/Components/Tasks/Tasks.js
+++ client/src/Components/Tasks/Tasks.js
@@ -28,24 +28,58 @@ class Tasks extends Component {
         }
         
         this.deleteTask = this.deleteTask.bind(this)
-        this.openModal = this.openModal.bind(this)
+      
         this.afterOpenModal = this.afterOpenModal.bind(this)
         this.closeModal = this.closeModal.bind(this)
+        this.getInitialState = this.getInitialState.bind(this)
+     
     }
 
     componentDidMount(props){
         this.props.getTasks()
+        this.getInitialState();
     }
-    // componentWillReceiveProps(nextProps){
-    //     this.setState({
-    //         tasks: [...this.state.tasks, nextProps.tasks]
-    //     })
-    // }
+    
 
     completeTask(id){
         this.props.completeTask(id)
         
     }
+
+    getInitialState(){
+
+        let boolean = false;
+
+        if(localStorage.getItem('modalOpen') === 'true'){
+            boolean = true
+        }
+
+        this.setState({
+            modalIsOpen: boolean,
+            selectedId: localStorage.getItem( 'selectedId' ),
+            selectedIndex: localStorage.getItem( 'selectedIndex' ),
+            selectedTitle: localStorage.getItem( 'selectedTitle' ),
+            selectedDescription: localStorage.getItem( 'description' )
+              });
+    }
+
+    setSelectedToStorage(id, i, title, description, boolean){
+        localStorage.setItem('selectedId', id)
+        localStorage.setItem('selectedIndex', i)
+        localStorage.setItem('selectedTitle', title)
+        localStorage.setItem('description', description)
+        localStorage.setItem('modalOpen', boolean)
+
+        this.setState({
+            modalIsOpen: boolean,
+            selectedId: id,
+            selectedIndex: i,
+            selectedTitle: title,
+            selectedDescription: description
+              });
+    }
+
+ 
     
 
     deleteTask(id){
@@ -55,16 +89,7 @@ class Tasks extends Component {
       }
 
       
-      openModal(id, i, title, description) {
-        
-            this.setState({modalIsOpen: true,
-                            selectedId: id,
-                            selectedIndex: i,
-                            selectedTitle: title,
-                            selectedDescription: description
-                              });
-        
-      }
+      
 
       afterOpenModal() {
         
@@ -72,7 +97,21 @@ class Tasks extends Component {
       }
 
       closeModal() {
-        this.setState({modalIsOpen: false});
+
+        localStorage.setItem('selectedId', 0)
+        localStorage.setItem('selectedIndex', 0)
+        localStorage.setItem('selectedTitle', '')
+        localStorage.setItem('description', '')
+        localStorage.setItem('modalOpen', false)
+
+        this.setState({
+            modalIsOpen: false,
+            selectedId: 0,
+            selectedIndex: 0,
+            selectedTitle: '',
+            selectedDescription: ''
+              });
+       
       }
 
     style1 = {
@@ -87,12 +126,13 @@ class Tasks extends Component {
         console.log('selectedID', this.state.selectedId)
         console.log('selectedTitle', this.state.selectedTitle)
         console.log('selectedDescription', this.state.selectedDescription)
+        console.log('modal storage', localStorage.getItem("modalOpen"))
 
         let tasks = this.props.tasks.map((task, i) => {
             return(
                 <div key={i}>
                     <div className='taskBox'>
-                        <div onClick={() => {this.openModal(task.id, i, task.title, task.description)}} className='taskTitleAndDesc'>
+                        <div onClick={() => {this.setSelectedToStorage(task.id, i, task.title, task.description, true)}} className='taskTitleAndDesc'>
                         <p className={task.completed ? 'taskBoxComplete' : 'notComplete'}>Task: {task.title}</p>
                         <p className={task.completed ? 'taskBoxComplete' : 'notComplete'}>Description: {task.description}</p>
 
@@ -131,7 +171,7 @@ class Tasks extends Component {
 
                         <button onClick={() => {this.props.updateTask(this.state.selectedId, this.state.selectedTitle, this.state.selectedDescription); this.closeModal() }}>Save</button>
 
-                        <button onClick={() => {this.openModal(this.props.tasks[this.state.selectedIndex].id, this.props.tasks[this.state.selectedIndex], this.props.tasks[this.state.selectedIndex].title, this.props.tasks[this.state.selectedIndex].description); this.closeModal()}}>cancel</button>
+                        <button onClick={() => {this.setSelectedToStorage(this.props.tasks[this.state.selectedIndex].id, this.props.tasks[this.state.selectedIndex], this.props.tasks[this.state.selectedIndex].title, this.props.tasks[this.state.selectedIndex].description); this.closeModal()}}>cancel</button>
 
                         <button onClick={() => {this.deleteTask(this.state.selectedId); this.closeModal()}} >delete</button>
 
